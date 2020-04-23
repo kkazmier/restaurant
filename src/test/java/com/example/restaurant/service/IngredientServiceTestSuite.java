ed460363b17cbdd55658977a856a521a168c61a9
kkazm <s14005@pjwstk.edu.pl>
2020-04-23 17:42:47 +0200

    problem with beans

---
+++ src/test/java/com/example/restaurant/service/IngredientServiceTestSuite.java
@@ -0,0 +1,34 @@
+package com.example.restaurant.service;
+
+import com.example.restaurant.domain.Ingredient;
+import com.example.restaurant.domain.dto.IngredientDto;
+import com.example.restaurant.mapper.IngredientMapper;
+import com.example.restaurant.repository.IngredientRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(IngredientService.class)
+public class IngredientServiceTestSuite {
+    @Autowired
+    IngredientService service;
+
+    @Autowired
+    IngredientRepository repository;
+
+    @Autowired
+    IngredientMapper mapper;
+
+    @Test
+    public void testFindIngredientById() {
+        Ingredient ingredient = mapper.mapToIngredient(
+                new IngredientDto(1L, "test", "test", 123.456, "?", "", 1L));
+        repository.save(ingredient);
+        assertThat(service.getIngredientById(1L)).isEqualTo(ingredient);
+    }
+}
