27cbad9f025f6c67c474c5d663d263b71151f36a
kkazm <s14005@pjwstk.edu.pl>
2020-06-20 21:24:34 +0200

    set naming strategy

--- src/main/java/com/example/restaurant/Configuration/DBConfiguration.java
+++ src/main/java/com/example/restaurant/Configuration/DBConfiguration.java
@@ -1,8 +1,14 @@
 package com.example.restaurant.Configuration;
 
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DBConfiguration {
-
+    @Bean
+    public PhysicalNamingStrategy physical() {
+        return new PhysicalNamingStrategyStandardImpl();
+    }
 }
