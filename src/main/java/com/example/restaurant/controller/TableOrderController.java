c1df8cad6fe36e76ce4bd2b5176f330fb68272d5
kkazm <s14005@pjwstk.edu.pl>
2020-07-01 19:23:24 +0200

    get all table-orders method

--- src/main/java/com/example/restaurant/controller/TableOrderController.java
+++ src/main/java/com/example/restaurant/controller/TableOrderController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tableOrder/")
@@ -24,6 +25,12 @@ public class TableOrderController {
 
     private TableOrderService tableOrderService;
     private DishService dishService;
+
+    @GetMapping(value = "all")
+    public List<TableOrder> getAllOrders(){
+        logger.info("Try to get all orders.");
+        return tableOrderService.getAllTableOrders();
+    }
 
     @PostMapping(value = "create", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createTableOrder(@RequestBody TableOrder tableOrder) throws URISyntaxException, Exception {
@@ -36,8 +43,24 @@ public class TableOrderController {
         return ResponseEntity.created(new URI("/v1/tableOrder/create")).body(result);
     }
 
+    @PostMapping(value = "addDish/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addDish(
+            @RequestParam("id") Long id,
+            @RequestBody Dish dish)
+            throws Exception {
+        logger.info("Try add dish to table order");
+        TableOrder order = tableOrderService.getTableOrderById(id).orElseThrow(Exception::new);
+        dish.setOrder(order);
+        order.getDishes().add(dish);
+        tableOrderService.saveTableOrder(order);
+        dishService.saveDish(dish);
+    }
+
     @RequestMapping(value = "addDish/{dishId}/toTableOrder/{orderId}")
-    public void addDishToOrder(@RequestParam("dishId") Long dishId, @RequestParam("orderId") Long orderId) throws Exception {
+    public void addDishToOrder(
+            @RequestParam("dishId") Long dishId,
+            @RequestParam("orderId") Long orderId)
+            throws Exception {
         logger.info("Try add dish to table order");
         Dish dish = dishService.getDishById(dishId).orElseThrow(Exception::new);
         TableOrder order = tableOrderService.getTableOrderById(orderId).orElseThrow(Exception::new);
