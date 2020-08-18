d851c0ff3f36447fc64398b2336baa3623319ce5
kkazm <s14005@pjwstk.edu.pl>
2020-08-18 20:40:43 +0200

    Little clean code.

--- src/main/java/com/example/restaurant/RestaurantApplication.java
+++ src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -1,11 +1,9 @@
 package com.example.restaurant;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import scenarios.scenario1.IngredientsAndDishes;
 
 @SpringBootApplication
 public class RestaurantApplication {
