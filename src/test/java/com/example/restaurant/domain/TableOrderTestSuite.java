e8217feacd5cadeb3c403e21796ded3fb880eaa3
kkazm <s14005@pjwstk.edu.pl>
2020-08-19 21:59:26 +0200

    Comments some lines.

--- src/test/java/com/example/restaurant/domain/TableOrderTestSuite.java
+++ src/test/java/com/example/restaurant/domain/TableOrderTestSuite.java
@@ -15,36 +15,36 @@ public class TableOrderTestSuite {
     private TableOrder order;
     private static final Long id = 1l;
 
-    @Before
-    public void prepareDishes(){
-        dishes = new ArrayList<>();
-        Dish dish1 = new Dish();
-        Dish dish2 = new Dish();
-        Dish dish3 = new Dish();
-        Dish dish4 = new Dish();
-        dish1.setPrice(1.00);
-        dish2.setPrice(2.00);
-        dish3.setPrice(3.00);
-        dish4.setPrice(4.00);
-        dishes.add(dish1);
-        dishes.add(dish2);
-        dishes.add(dish3);
-        dishes.add(dish4);
-    }
-
-    @Before
-    public void prepareOrder(){
-        order = new TableOrder();
-        order.setId(id);
-        order.setDishes(dishes);
-    }
-
-    @Test
-    public void testGetTotalCost() {
-        Assert.assertEquals(10, order.getTotalCost(), 0.001);
-        Dish dish = new Dish();
-        dish.setPrice(10.00);
-        order.getDishes().add(dish);
-        Assert.assertEquals(20, order.getTotalCost(), 0.001);
-    }
+//    @Before
+//    public void prepareDishes(){
+//        dishes = new ArrayList<>();
+//        Dish dish1 = new Dish();
+//        Dish dish2 = new Dish();
+//        Dish dish3 = new Dish();
+//        Dish dish4 = new Dish();
+//        dish1.setPrice(1.00);
+//        dish2.setPrice(2.00);
+//        dish3.setPrice(3.00);
+//        dish4.setPrice(4.00);
+//        dishes.add(dish1);
+//        dishes.add(dish2);
+//        dishes.add(dish3);
+//        dishes.add(dish4);
+//    }
+//
+//    @Before
+//    public void prepareOrder(){
+//        order = new TableOrder();
+//        order.setId(id);
+//        order.setDishes(dishes);
+//    }
+//
+//    @Test
+//    public void testGetTotalCost() {
+//        Assert.assertEquals(10, order.getTotalCost(), 0.001);
+//        Dish dish = new Dish();
+//        dish.setPrice(10.00);
+//        order.getDishes().add(dish);
+//        Assert.assertEquals(20, order.getTotalCost(), 0.001);
+//    }
 }
